93f9f2033147df5c8af22b934767b374ce06a36d
micheaol <micheaol80@gmail.com>
2022-05-25 17:29:23 +0100

    Add modal to the register component

--- components/common/auth/register/index.jsx
+++ components/common/auth/register/index.jsx
@@ -4,6 +4,7 @@ import Image from 'next/image';
 import TextInput from '../../../widgets/appTextInput';
 import AppButton from "../../../widgets/buttons/AppButton";
 import Selectitem from '../../../widgets/appSelect/appSelect';
+import { Button, Modal } from 'react-bootstrap';
 
 import {API} from '../../../../pages/api/client-side/fetcher';
 
@@ -72,5 +73,35 @@ const Register = (props) => {
     </>
   )
 }
+
+function RegisterModal() {
+  const values = [true, 'sm-down', 'md-down', 'lg-down', 'xl-down', 'xxl-down'];
+  const [fullscreen, setFullscreen] = useState(true);
+  const [show, setShow] = useState(false);
+
+  function handleShow(breakpoint) {
+    setFullscreen(breakpoint);
+    setShow(true);
+  }
+
+  return (
+    <>
+      {values.map((v, idx) => (
+        <Button key={idx} className="me-2 mb-2" onClick={() => handleShow(v)}>
+          Full screen
+          {typeof v === 'string' && `below ${v.split('-')[0]}`}
+        </Button>
+      ))}
+      <Modal show={show} fullscreen={fullscreen} onHide={() => setShow(false)}>
+        <Modal.Header closeButton>
+          <Modal.Title>Modal</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>Modal body content</Modal.Body>
+      </Modal>
+    </>
+  );
+}
+
+
 
 export default Register
