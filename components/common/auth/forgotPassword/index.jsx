cd9d9c64cc951902fd4d0c8aa040f2b2a3845efc
Ogundipe Abimbola <ogundipeabimbola9@gmail.com>
2022-05-31 15:46:54 -0800

    reponsiveness for Ipad

--- components/common/auth/forgotPassword/index.jsx
+++ components/common/auth/forgotPassword/index.jsx
@@ -6,9 +6,9 @@ import TextInput from '../../../widgets/appTextInput';
 
 const Login = () => {
   return (
-    <div className="row">
+<>
     <div className={styles.floatImg1}><Image alt={"design image"} src={'/assets/img/common/login/HalfCircleBlackk.png'} width={86} height={200}/></div>
-    <div className={styles.floatImg2}><Image alt={"design image"} src={'/assets/img/common/login/HalfCircleWhite.png'} width={150} height={90}/></div>
+    <div className={styles.floatImg2}><Image alt={"design image"} src={'/assets/img/common/login/HalfCircleWhite.png'}   width={150} height={90}/></div>
     <div className={styles.floatImg3}><Image alt={"design image"} src={'/assets/img/common/login/HalfCircleWhite.png'} width={150} height={90}/></div>
       <div className='row' >
         <div className={`col-md-4 ${styles.resetContainer}`}>   
@@ -26,7 +26,7 @@ const Login = () => {
         <div >  
         <p className={`center ${styles.afterSocialText}`}>Back to <Link passHref href='/login'><b>Log In</b></Link></p> </div>
       </div>      
-    </div>
+      </>
   )
 }
 
