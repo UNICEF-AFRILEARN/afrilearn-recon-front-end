cc585178c9e305f4d147bcd8073ba0c60e6286a3
micheaol <micheaol80@gmail.com>
2022-05-31 12:16:00 +0100

    A styling to the why learn component

--- components/common/homepage/extra/profileSection.jsx
+++ components/common/homepage/extra/profileSection.jsx
@@ -11,18 +11,18 @@ const ProfileSection = ({data}) => {
             return <p key={item}><Image alt={"design image"} src='/assets/img/common/homepage/Check.png' width={25} height={25}/><span className={styles.bulletText}>{item}</span></p>
           })}        
           <section className={`${styles.mobileDownloadSection} button-setions`}>
-          {/* <div className="col-md-4 pt-3"> */}
-                <span className={` btn-class-corrected ${styles.btnJoinForFree}`}><Link passHref href='/'>JOIN FOR FREE</Link></span>
-              {/* </div> */}
+          <div className="join-btn-wrapper">
+                <span className={` btn-class-corrected`}><Link passHref href='/'>JOIN FOR FREE</Link></span>
+              </div>
               <div className="play-store-class">
               <div className="class-play-store">
               <Link passHref href="https://play.google.com/store/apps/details?id=com.afrilearn">
-                <Image alt={"design image"} src='/assets/img/common/homepage/Google Play Badge.png' width={150} height={45}/> 
+                <Image alt={"design image"} src='/assets/img/common/homepage/Google Play Badge.png' width={150} height={55}/> 
               </Link>             
             </div>
             <div className="class-play-store">            
               <Link passHref href="https://apps.apple.com/ng/app/afrilearn/id1587978653">
-                <Image alt={"design image"} src='/assets/img/common/homepage/App Store Badge.png' width={150} height={45}/> 
+                <Image alt={"design image"} src='/assets/img/common/homepage/App Store Badge.png' width={150} height={55}/> 
               </Link>
             </div>
           </div>
