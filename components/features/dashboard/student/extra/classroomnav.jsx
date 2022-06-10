1ab322961805c1e131e3853acb84ceabfa18c1ae
micheaol <micheaol80@gmail.com>
2022-06-10 15:42:57 +0100

    Create classroom component

---
+++ components/features/dashboard/student/extra/classroomnav.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Classroomnav = () => {
+  return (
+    <div>
+        <h1>classroomnav</h1>
+    </div>
+  )
+}
+
+export default Classroomnav
---
+++ pages/classroom.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import Image from 'next/image';
+import  styles  from "../styles/videolesson.module.css"
+import Classroomnav from '../components/features/dashboard/student/extra/classroomnav';
+
+const Classroom = () => {
+  return (
+    <div className={styles.maincotainer}>
+        <div className={styles.videoheropage}>
+              <div className={styles.heroheader}>
+                <Classroomnav />
+              </div>
+              <div className={styles.subheadings}>
+                  <Image
+              src="/assets/img/classavatar.svg"
+              alt="teacher"
+              width={200}
+              height={200}
+            />
+              </div>
+        </div>
+    </div>
+  )
+}
+
+export default Classroom
