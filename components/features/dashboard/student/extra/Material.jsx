ebb7ef2b00662c5f8c98df915cd89e13ffc7a048
micheaol <micheaol80@gmail.com>
2022-06-13 22:49:07 +0100

    Add material segment

--- components/features/dashboard/student/extra/Material.jsx
+++ components/features/dashboard/student/extra/Material.jsx
@@ -1,9 +1,171 @@
-import React from 'react'
+import React from 'react';
+import Image from 'next/image';
+import styles from "../../../../../styles/material.module.css";
 
 const Material = () => {
   return (
-    <div>
-        <h1>Material</h1>
+    <div className={styles.classworkwrapper}>
+
+        <nav className={styles.sidenavbar}>
+          <h5>All Subjects</h5>
+          <ul>
+            <li>Mathematics</li>
+            <li>English Language</li>
+            <li>Business Studies</li>
+            <li>Computer Science</li>
+            <li>CRK</li>
+            <li>Basic Technology</li>
+            <li>French</li>
+            <li>Basic Science</li>
+            <li>Yoruba</li>
+          </ul>
+        </nav>
+        <div className={styles.classdetailswrapper}>
+              <div className={styles.subjectwrapper}>
+                <h3>Junior WAEC</h3>
+              </div>
+              <div className={styles.subjectdetails}>
+                <div className={styles.innersubject}>
+             
+                    <p>Computer Science</p><span>Select Year
+                    <Image
+                      src="/assets/img/downarrow.svg"
+                      alt="teacher"
+                      width={40}
+                      height={10}
+                    />
+                    </span>
+                </div>
+                
+              </div>
+              <div className={styles.subjectdetails}>
+                <div className={styles.innersubject}>
+               
+                <p>Computer Science</p><span>Select Year
+                    <Image
+                      src="/assets/img/downarrow.svg"
+                      alt="teacher"
+                      width={40}
+                      height={10}
+                    />
+                    </span>
+                   
+                </div>
+                
+              </div>
+              <div className={styles.subjectdetails}>
+                <div className={styles.innersubject}>
+               
+                <p>Computer Science</p><span>Select Year
+                    <Image
+                      src="/assets/img/downarrow.svg"
+                      alt="teacher"
+                      width={40}
+                      height={10}
+                    />
+                    </span>
+                    
+                </div>
+              
+              </div>
+              <div className={styles.subjectwrapper}>
+                <h3>Mathematics</h3>
+              </div>
+              <div className={styles.subjectdetails}>
+                <div className={styles.innersubject}>
+               
+                <p>Computer Science</p><span>Select Year
+                    <Image
+                      src="/assets/img/downarrow.svg"
+                      alt="teacher"
+                      width={40}
+                      height={10}
+                    />
+                    </span>
+                    
+                </div>
+                
+              </div>
+              <div className={styles.subjectdetails}>
+                <div className={styles.innersubject}>
+              
+                <p>Computer Science</p><span>Select Year
+                    <Image
+                      src="/assets/img/downarrow.svg"
+                      alt="teacher"
+                      width={40}
+                      height={10}
+                    />
+                    </span>
+                    
+                </div>
+                
+              </div>
+              <div className={styles.subjectdetails}>
+                <div className={styles.innersubject}>
+                
+                <p>Computer Science</p><span>Select Year
+                    <Image
+                      src="/assets/img/downarrow.svg"
+                      alt="teacher"
+                      width={40}
+                      height={10}
+                    />
+                    </span>
+                    
+                </div>
+              
+              </div>
+              <div className={styles.subjectwrapper}>
+                <h3>Mathematics</h3>
+              </div>
+              <div className={styles.subjectdetails}>
+                <div className={styles.innersubject}>
+               
+                <p>Computer Science</p><span>Select Year
+                    <Image
+                      src="/assets/img/downarrow.svg"
+                      alt="teacher"
+                      width={40}
+                      height={10}
+                    />
+                    </span>
+                    
+                </div>
+                
+              </div>
+              <div className={styles.subjectdetails}>
+                <div className={styles.innersubject}>
+                
+                <p>Computer Science</p><span>Select Year
+                    <Image
+                      src="/assets/img/downarrow.svg"
+                      alt="teacher"
+                      width={40}
+                      height={10}
+                    />
+                    </span>
+                    
+                </div>
+                
+              </div>
+              <div className={styles.subjectdetails}>
+                <div className={styles.innersubject}>
+               
+                <p>Computer Science</p><span>Select Year
+                    <Image
+                      src="/assets/img/downarrow.svg"
+                      alt="teacher"
+                      width={40}
+                      height={10}
+                    />
+                    </span>
+                    
+                </div>
+              
+              </div>
+        </div>
+        
     </div>
   )
 }
