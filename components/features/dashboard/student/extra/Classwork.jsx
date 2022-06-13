30f946c40534704ddfe853c64ab2f2d86be517a2
micheaol <micheaol80@gmail.com>
2022-06-13 16:28:21 +0100

    Add classword seg

--- components/features/dashboard/student/extra/Classwork.jsx
+++ components/features/dashboard/student/extra/Classwork.jsx
@@ -1,9 +1,66 @@
-import React from 'react'
+import React from 'react';
+import Image from 'next/image';
+import styles from "../../../../../styles/classwork.module.css";
 
 const Classwork = () => {
   return (
-    <div>
-        <h1>Classwork</h1>
+    <div className={styles.classworkwrapper}>
+
+        <nav className={styles.sidenavbar}>
+          <h5>All Subjects</h5>
+          <ul>
+            <li>Mathematics</li>
+            <li>English Language</li>
+            <li>Business Studies</li>
+            <li>Computer Science</li>
+            <li>CRK</li>
+            <li>Basic Technology</li>
+            <li>French</li>
+            <li>Basic Science</li>
+            <li>Yoruba</li>
+          </ul>
+        </nav>
+        <div className={styles.classdetailswrapper}>
+              <div className={styles.subjectwrapper}>
+                <h3>Mathematics</h3>
+              </div>
+              <div className={styles.subjectdetails}>
+                <div className={styles.innersubject}>
+                <Image
+              src="/assets/img/proavatar.svg"
+              alt="teacher"
+              width={50}
+              height={50}
+            />
+                    <p>Attached is the link,complete the video lesson for ...</p><span>Due 06 Sept 2020</span>
+                </div>
+                <span>Posted: 02 Sept 2020</span>
+              </div>
+              <div className={styles.subjectdetails}>
+                <div className={styles.innersubject}>
+                <Image
+              src="/assets/img/proavatar.svg"
+              alt="teacher"
+              width={50}
+              height={50}
+            />
+                    <p>Attached is the link,complete the video lesson for ...</p><span>Due 06 Sept 2020</span>
+                </div>
+                <span>Posted: 02 Sept 2020</span>
+              </div>
+              <div className={styles.subjectdetails}>
+                <div className={styles.innersubject}>
+                <Image
+              src="/assets/img/proavatar.svg"
+              alt="teacher"
+              width={50}
+              height={50}
+            />
+                    <p>Attached is the link,complete the video lesson for ...</p><span>Due 06 Sept 2020</span>
+                </div>
+                <span>Posted: 02 Sept 2020</span>
+              </div>
+        </div>
     </div>
   )
 }
