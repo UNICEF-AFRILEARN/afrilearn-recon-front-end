2e1636b8dd3ae7798fa09e8bdbb01242f2d260a6
micheaol <micheaol80@gmail.com>
2022-06-13 13:44:30 +0100

    Add active tabs to the classroom

--- components/features/dashboard/student/extra/classroomnav.jsx
+++ components/features/dashboard/student/extra/classroomnav.jsx
@@ -1,20 +1,38 @@
 import Link from 'next/link';
-import React from 'react';
+import React, { useState } from 'react';
 import styles from "../../../../../styles/classroom.module.css";
 import Announcesment from './announcesment';
 
-const Classroomnav = ({props}) => {
-  console.log(props)
+const Classroomnav = () => {
+  const [toggleState, setToggleState] = useState(1);
+
+  const toggleTab = (index) => {
+    setToggleState(index);
+  }
   return (
     <div className={styles.maincontainer}>
         <nav className={styles.navbar}>
           <ul>
-            <li>Announcements</li>
-            
-            <li>Materials</li>
-            <li>Classwork</li>
-            <li>People</li>
-            <li>Class Performance</li>
+            <li 
+            onClick={() => toggleTab(1)}
+            className={toggleState === 1 ? styles.activetab : styles.tabs}
+            >Announcements</li>
+            <li 
+            onClick={() => toggleTab(2)}
+            className={toggleState === 2 ? styles.activetab : styles.tabs}
+            >Materials</li>
+            <li 
+            onClick={() => toggleTab(3)}
+            className={toggleState === 3 ? styles.activetab : styles.tabs}
+            >Classwork</li>
+            <li 
+            onClick={() => toggleTab(4)}
+            className={toggleState === 4 ? styles.activetab : styles.tabs}
+            >People</li>
+            <li 
+            onClick={() => toggleTab(5)}
+            className={toggleState === 5 ? styles.activetab : styles.tabs}
+            >Class Performance</li>
           </ul>
         </nav>
 
