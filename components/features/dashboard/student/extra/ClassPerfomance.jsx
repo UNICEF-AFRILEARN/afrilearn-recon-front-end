1f0261ad3609427417d53c9f85f3337f85f7237b
micheaol <micheaol80@gmail.com>
2022-06-15 07:51:42 +0100

    Add style

--- components/features/dashboard/student/extra/ClassPerfomance.jsx
+++ components/features/dashboard/student/extra/ClassPerfomance.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const ClassPerfomance = () => {
   return (
-    <div>
-        <h1>ClassPerfomance</h1>
+    <div className={styles.performancemainwrapper}>
+        
     </div>
   )
 }
