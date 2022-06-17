e24ebbfa25ea6a8d1f3b37d7313ed2ee4fd5a213
micheaol <micheaol80@gmail.com>
2022-06-17 15:07:58 +0100

    Add charts to the project

--- components/features/dashboard/student/extra/ClassPerfomance.jsx
+++ components/features/dashboard/student/extra/ClassPerfomance.jsx
@@ -1,10 +1,24 @@
 import React from 'react';
 import styles from '../../../../../styles/performance.module.css';
+import PieChartSection from './PieChartSection';
 
 const ClassPerfomance = () => {
   return (
     <div className={styles.performancemainwrapper}>
-        
+      <div className={styles.leftwrapper}>
+          <div className={styles.studentnamewrapper}>
+              <h2>Alaka Feyikemi</h2>
+                <p>feyikemi199@gmail.com</p>
+              <h3>JSS 1</h3>
+          </div>
+          <div className={styles.progresswrapper}>
+              <h1>Progress</h1>
+          </div>
+      </div>
+      <div className={styles.piewrapper}>
+        <h1>het</h1>
+        <PieChartSection />
+      </div>
     </div>
   )
 }
--- components/features/dashboard/student/extra/announcesment.jsx
+++ components/features/dashboard/student/extra/announcesment.jsx
@@ -1,5 +1,5 @@
-import Image from 'next/image';
 import React from 'react';
+import Image from 'next/image';
 import styles from "../../../../../styles/announcement.module.css";
 
 const Announcesment = () => {
