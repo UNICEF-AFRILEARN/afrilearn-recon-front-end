a902f6249810210a762a8ef883ab1fcb4655d380
Ogundipe Abimbola <ogundipeabimbola9@gmail.com>
2022-07-05 13:03:35 -0800

    QUIZ DIAGRAM DIV

--- pages/quiz/extra/quizQuestion.jsx
+++ pages/quiz/extra/quizQuestion.jsx
@@ -13,7 +13,8 @@ const QuizQuestion = () => {
     questionNum : "Question 28",
     question : "Which angle is represented in the diagram  shown above?",
     option : [
-    ]
+    ],
+    diagram: ''
       }
       const router = useRouter()
       return ( 
@@ -22,6 +23,7 @@ const QuizQuestion = () => {
         <div className={styles.QuizNumberSubject}><h4>{Quizdata?.subject}</h4></div>
         <div className={styles.QuizNumberQuestionNum}><p>{Quizdata?.questionNum}</p></div>
         <div className={styles.QuizNumberQuestion}><p>{Quizdata?.question}</p></div>
+        <div>{Quizdata?.diagram}</div>
         <div className={styles.options}>
           <div className={styles.quizOption}><button>A . {}</button></div>
           <div className={styles.quizOption}><button>B . {}</button></div>
