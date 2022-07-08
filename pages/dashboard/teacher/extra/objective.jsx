0460bb46c695b4dd065a5d30b94b68227845ecaa
Michael <micheaol80@gmail.com>
2022-07-08 13:31:58 +0100

    Add theory question panel

--- pages/dashboard/teacher/extra/objective.jsx
+++ pages/dashboard/teacher/extra/objective.jsx
@@ -1,11 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from '../../../../styles/teacher.module.css'; 
 import { BsFillCircleFill, BsCircle } from 'react-icons/bs';
 import { RiDeleteBin6Line } from 'react-icons/ri';
 import { BiNote } from 'react-icons/bi';
 import Questionpanel from './questionpanel';
+import Theory from './theory';
 
 const Objectives = () => {
+    const [showObjQuestions, setShowObjQuestions] = useState(null)
+
+    const showObjpanel = (id) => {
+        setShowObjQuestions(id)
+    }
   return (
     <div className={styles.objectivemainwrapper}>
         <div className={styles.objleftsideboxwrapper}>
@@ -24,8 +30,8 @@ const Objectives = () => {
         </div>
         <div className={styles.examsetupwrapper}>
             <div className={styles.xamssetpheader}>
-            <h4>Objective</h4>
-            <h5>Theory</h5>
+            <h4 onClick={() => showObjpanel(1)}>Objective</h4>
+            <h5 onClick={() => showObjpanel(2)}>Theory</h5>
             </div>
             <div className={styles.classlistwrapper}>
                 <div className={styles.innerclasslistwrapper}>
@@ -44,8 +50,8 @@ const Objectives = () => {
                     </ul>    
                 </div>
             </div>
-                <Questionpanel />
-
+               { showObjQuestions === 1 &&  <Questionpanel />}
+                {showObjQuestions === 2 && <Theory /> }
         </div>
         
     </div>
