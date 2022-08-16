8bb8c031082b3231cdfe5086eeadc62e01109f79
Michael <micheaol80@gmail.com>
2022-08-16 03:26:10 +0100

    Add child courses based on the class selected

--- pages/dashboard/parent/extra/classcontentcard.jsx
+++ pages/dashboard/parent/extra/classcontentcard.jsx
@@ -10,9 +10,38 @@ import {
 import styles from '../../../../styles/parentdashboard.module.css';
 
 
-const Classcontentcard = () => {
+const Classcontentcard = ({contentId, course_sorted}) => {
   const [selectedItem, setSelectedItem] = useState(null)
 
+  console.log("contentId*****", contentId)
+
+
+  //function to extract ids and name:
+  const get_child_course_sorted = (messages) => {
+    return messages?.filter((obj) => {
+      return obj.mainSubjectId && obj.mainSubjectId.id === contentId ;
+      })
+      .map((obj) =>{
+        return obj.relatedLessons;
+      });
+  }
+
+  const get_student_courses_sorted = () => {
+    let student_sorted_courses = get_child_course_sorted(course_sorted);
+    for(let i = 0; i < student_sorted_courses.length; i++){
+      for(let j = 0; j < student_sorted_courses.length; j++){
+        
+        console.log("student_sorted_courses ***", student_sorted_courses[i][j])
+      }
+
+    }
+
+  }
+
+  get_student_courses_sorted()
+
+
+      
   const toggleItem = (id) => {
     if(selectedItem === id){
       return setSelectedItem(null)
@@ -25,7 +54,11 @@ const Classcontentcard = () => {
     <div className={styles.cardcontentwrapper}>
       <div className={styles.innercontentwrapper} onClick={() => toggleItem(1)}>
       <div className={styles.contentquestionwrapper}>
-        <h4> <BsCheck className={styles.checkbg} color="white"/> Geometrical Construction (1):  Lines <span>{selectedItem === 1 ? <MdOutlineKeyboardArrowDown size={30} /> : <MdOutlineKeyboardArrowUp size={30}/>}</span></h4>
+        {/* {student_sorted_courses && student_sorted_courses.map((mapped_courses) =>
+          <h4> <BsCheck className={styles.checkbg} color="white"/> {console.log("mapped_courses", mapped_courses)} (1):  Lines <span>{selectedItem === 1 ? <MdOutlineKeyboardArrowDown size={30} /> : <MdOutlineKeyboardArrowUp size={30}/>}</span></h4>
+        )
+
+        } */}
         </div>
         <div className={ selectedItem === 1  ? styles.contentshow :  styles.contenthidewrapper}>
           <ul>
--- pages/dashboard/parent/extra/classcontentmain.jsx
+++ pages/dashboard/parent/extra/classcontentmain.jsx
@@ -12,7 +12,8 @@ const Classcontentmain = ({myChildren, course}) => {
   const dispatch = useDispatch();
 
   const getContentIds = (id) =>{
-    console.log("e.target.id", id)
+    setContentId(id)
+    console.log("contentId from coursecontent==", contentId)
   }
 
 
@@ -28,7 +29,7 @@ const Classcontentmain = ({myChildren, course}) => {
                {course_sorted && course_sorted?.map((course_sorted) => <li key={course_sorted.mainSubjectId.id} onClick={() => getContentIds(course_sorted.mainSubjectId.id)} >{course_sorted.mainSubjectId.name}</li>) }
             </ul>    
         </div>
-        <Classcontentcard />
+        <Classcontentcard contentId={contentId} course_sorted={course_sorted}/>
     </div>
   )
 }
