4de86c567026bbf4a49e780c47cf160db7722602
Michael <micheaol80@gmail.com>
2022-08-24 20:36:05 +0100

    Update middle avbar

--- components/features/dashboard/school/extra/middleNavbar.jsx
+++ components/features/dashboard/school/extra/middleNavbar.jsx
@@ -9,13 +9,13 @@ const MiddleNavbar = () => {
   const { user, registerUser, roles } = useSelector((state) => state.auth);
   const dispatch = useDispatch();
 
-  console.log("registerUser from middlenaV=>", registerUser);
+  console.log("schoolProfile from middlenaV=>", schoolProfile);
   const schoolId = user.user?.schoolId.id || registerUser.user?.schoolId.id
   console.log("schoolId from middlenaV=>", schoolId);
 
   useEffect(() => {
     dispatch(fetchSchoolProfileInitiate(schoolId))
-  },[])
+  },[schoolId])
   return (
     
     <div className={styles.middlenavmainwrapper}>
@@ -24,11 +24,11 @@ const MiddleNavbar = () => {
         </div>
         <div className={styles.middleinnerwrapper}>
             <h4> Teacher</h4>
-            <h3>0{schoolProfile?.data?.numOfTeachers}</h3>
+            <h3>0{schoolProfile?.numOfTeachers}</h3>
         </div>
         <div className={styles.middleinnerwrapper}>
             <h4> Students</h4>
-            <h3>0{schoolProfile?.data?.numOfStudents}</h3>
+            <h3>0{schoolProfile?.numOfStudents}</h3>
         </div>
     </div>
 
