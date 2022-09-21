737117631a6d4bf261f55ca37c5d7d1ee2e88975
Michael <micheaol80@gmail.com>
2022-09-21 10:36:38 +0100

    Add logic to display overall performance

--- pages/dashboard/performance/piechart/index.jsx
+++ pages/dashboard/performance/piechart/index.jsx
@@ -5,12 +5,19 @@ import { BarChart, Bar, Cell, XAxis, Label, YAxis, CartesianGrid, Tooltip, Legen
 
 
 const Piechart = ({data}) => {
+
     return (
       // <ResponsiveContainer width="50%" height="50%">
-        <BarChart width={400} height={350} data={data}>
+        <BarChart 
+        width={400} 
+        height={350} 
+        data={data}
+        margin={{top: 20, right: 30, left: 20, bottom: 165}}
+        >
           <Bar dataKey="progress" fill="#8884d8"
             radius={[20, 20, 20, 20]}
             barSize={28}
+
             // label={{ position: 'baseline'}}
 
             
@@ -21,11 +28,14 @@ const Piechart = ({data}) => {
           fontFamily="sans-serif"
           tickSize
           angle={-90}
-          dx={-10}
-          dy={15}
+          dx={-8}
+          dy={5}
           minTickGap={-200}
           axisLine={false}
           interval={0}
+          textAnchor="end"
+          scaleToFit={true}
+          orientation='bottom'
        />
        <YAxis hide/>
           
