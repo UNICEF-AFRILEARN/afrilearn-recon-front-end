dced513a1567f86e19104d68803f05c9f5275c46
Kiyoonewton <kiyoonewton41@gmail.com>
2022-10-07 15:22:05 +0100

    Color adjustment and responsiveness

--- components/features/dashboard/student/extra/announcesment.jsx
+++ components/features/dashboard/student/extra/announcesment.jsx
@@ -1,33 +1,33 @@
-import React, { useEffect, useState } from "react";
-import Image from "next/image";
-import styles from "../../../../../styles/announcement.module.css";
-import { useDispatch, useSelector } from "react-redux";
-import moment from "moment";
-import { Col, Row } from "react-bootstrap";
-import { createComment } from "../../../../../redux/actions/subject";
+import React, { useEffect, useState } from 'react'
+import Image from 'next/image'
+import styles from '../../../../../styles/announcement.module.css'
+import { useDispatch, useSelector } from 'react-redux'
+import moment from 'moment'
+import { Col, Row } from 'react-bootstrap'
+import { createComment } from '../../../../../redux/actions/subject'
 
 const Announcesment = ({ goto, classId }) => {
-  const { user } = useSelector((state) => state.auth);
-  const subject = useSelector((state) => state.mySubjectCourse);
-  const dispatch = useDispatch();
-  const clazz = subject?.announcement[2];
-  console.log(subject);
+  const { user } = useSelector((state) => state.auth)
+  const subject = useSelector((state) => state.mySubjectCourse)
+  const dispatch = useDispatch()
+  const clazz = subject?.announcement[2]
+  console.log(subject)
   const TeacherAnnouncement = ({ subject }) => {
-    const dispatch = useDispatch();
+    const dispatch = useDispatch()
     const capitalizeFirst = (str) => {
-      return str.charAt(0).toUpperCase() + str.slice(1);
-    };
+      return str.charAt(0).toUpperCase() + str.slice(1)
+    }
 
     return subject?.announcement[0]?.announcements.length > 0 ? (
       subject?.announcement[0]?.announcements.map((data, i) => {
-        const [comment, setComment] = useState("");
+        const [comment, setComment] = useState('')
         const sendComment = async (e, id) => {
-          e.preventDefault();
-          if (comment !== "") {
-            dispatch(createComment(id, comment, classId, user?.token));
-            setComment("");
+          e.preventDefault()
+          if (comment !== '') {
+            dispatch(createComment(id, comment, classId, user?.token))
+            setComment('')
           }
-        };
+        }
         return (
           <div key={i} className={styles.seconmessage}>
             <div className={styles.innermessage}>
@@ -40,13 +40,13 @@ const Announcesment = ({ goto, classId }) => {
                 alt="teacher"
                 width={50}
                 height={50}
-                style={{ borderRadius: "100px" }}
+                style={{ borderRadius: '100px' }}
               />
-              <div className={styles.teachertitle}>
-                <p style={{ color: "black" }}>
+              <div className={styles.teachertitled}>
+                <p style={{ color: 'black', margin: '0' }}>
                   Mr {data.teacher.fullName} (Teacher)
                 </p>
-                <span style={{ fontSize: "12px" }}>
+                <span style={{ fontSize: '12px' }}>
                   {moment(data.createdAt).fromNow()}
                 </span>
               </div>
@@ -55,16 +55,16 @@ const Announcesment = ({ goto, classId }) => {
               <p className="text-dark m-0">{capitalizeFirst(data.text)}</p>
             </div>
             <div className={styles.dividerclass}></div>
-            <div className="m-3">
+            <div className={`m-3 ${styles.noComment}`}>
               <div>
-                <p style={{ color: "#00D9B6" }}>
+                <p style={{ color: '#00D9B6' }}>
                   {data?.comments.length} class comment
                 </p>
               </div>
               {data?.comments.map((dts, i) =>
                 dts ? (
-                  <Row key={i} style={{ display: "flex", margin: "15px 20px" }}>
-                    <Col md={1} className="p-0">
+                  <Row key={i} style={{ display: 'flex', margin: '15px 20px' }}>
+                    <Col md={1} className={`p-0 ${styles.commentPic}`}>
                       <Image
                         src={
                           dts.student.profilePhotoUrl
@@ -75,19 +75,22 @@ const Announcesment = ({ goto, classId }) => {
                         width={50}
                         height={50}
                         style={{
-                          borderRadius: "100px",
-                          objectPosition: "100%",
+                          borderRadius: '100px',
+                          objectPosition: '100%',
                         }}
                       />
                     </Col>
-                    <Col className={`p-0 ${styles.commentimageconts}`}>
+                    <Col
+                      className={`ps-lg-0 ps-md-3 ${styles.commentimageconts}`}
+                    >
                       <div key={i} className={` ${styles.teachertitle}`}>
                         <p className="m-0 text-dark">
                           <span
                             style={{
-                              fontSize: "13px",
-                              marginLeft: "0",
+                              fontSize: '13px',
+                              marginLeft: '0',
                             }}
+                            className={` ${styles.teacherName}`}
                           >
                             {dts.student.id === data.teacher._id
                               ? `Mr ${dts.student.fullName} (Teacher)`
@@ -97,15 +100,16 @@ const Announcesment = ({ goto, classId }) => {
                           </span>
                           <span
                             style={{
-                              fontSize: "12px",
-                              color: "gray",
-                              marginLeft: "15px",
+                              fontSize: '12px',
+                              color: 'gray',
+                              marginLeft: '15px',
                             }}
+                            className={` ${styles.teacherdate}`}
                           >
                             {moment(dts.createdAt).fromNow()}
                           </span>
                         </p>
-                        <span className="m-0 text-dark">
+                        <span className={`m-0 text-dark ${styles.commentText}`}>
                           {capitalizeFirst(dts.text)}
                         </span>
                       </div>
@@ -115,7 +119,7 @@ const Announcesment = ({ goto, classId }) => {
                   <div key={i} className={styles.teachertitle}>
                     <p>No Comment yet</p>
                   </div>
-                ),
+                )
               )}
             </div>
             <div className={styles.dividerclass}></div>
@@ -131,7 +135,7 @@ const Announcesment = ({ goto, classId }) => {
                   />
                 </div>
                 <div
-                  style={{ cursor: "pointer" }}
+                  style={{ cursor: 'pointer' }}
                   className={`${styles.commentAvatarMRSide}`}
                   onClick={(e) => sendComment(e, data._id)}
                 ></div>
@@ -139,55 +143,72 @@ const Announcesment = ({ goto, classId }) => {
             </Col>
             <div className={`mt-2 ${styles.dividerclass}`}></div>
           </div>
-        );
+        )
       })
     ) : (
       <h6>No Announcement list yet</h6>
-    );
-  };
+    )
+  }
 
   const AssignContent = () => {
     const minDigit = clazz?.assignedContents.filter((data) => {
-      return data.audience === "all" || data.userId?.id === user?.user.id;
-    });
+      return data.audience === 'all' || data.userId?.id === user?.user.id
+    })
     const minsDigit = minDigit?.map((data) => {
-      return new Date(data.dueDate).getTime() - new Date().getTime();
-    });
+      return new Date(data.dueDate).getTime() - new Date().getTime()
+    })
     const minAssign =
-      minDigit && minDigit[minsDigit?.indexOf(Math.min(...minsDigit))];
-    console.log(minAssign);
+      minDigit &&
+      // minDigit > 0 &&
+      minDigit[minsDigit?.indexOf(Math.min(...minsDigit))]
+    console.log(minAssign)
     // return <p>ti moba tiri</p>;
     return minAssign ? (
       <>
-        <div className="item">
-          <Image
-            src="/assets/img/anounceImg.png"
-            alt="teacher"
-            width={50}
-            height={50}
-          />
-          <div>
-            <p>
+        <Row className="item">
+          <Col md={2} style={{ height: '50px' }}>
+            <Image
+              src="/assets/img/anounceImg.png"
+              alt="teacher"
+              width={50}
+              height={50}
+            />
+          </Col>
+          <Col style={{ fontSize: '13px' }}>
+            <span style={{ color: '#333' }}>{`Mr ${minAssign.teacher.fullName
+              .split(' ')
+              .slice(0, -1)
+              .join(' ')} (Teacher) posted a new ...`}</span>
+
+            <p className="m-0">
               Posted:&nbsp;
-              {moment(minAssign.createdAt).format("LL")}
+              <span style={{ color: 'gray', margin: '0', fontSize: '13px' }}>
+                {moment(minAssign.createdAt).format('LL')}
+              </span>
             </p>
             <p>
               Due Date:&nbsp;
-              {moment(minAssign.dueDate).format("LL")}
+              <span style={{ color: 'gray', margin: '0', fontSize: '13px' }}>
+                {moment(minAssign.dueDate).format('LL')}
+              </span>
             </p>
-          </div>
-        </div>
-        <a onClick={() => goto(3)} style={{ marginRigth: "auto" }}>
-          <u style={{ color: "#00D9B6", marginRigth: "0" }}>View All</u>
+          </Col>
+        </Row>
+        <a onClick={() => goto(3)} style={{ marginRigth: 'auto' }}>
+          <u
+            style={{ color: '#00D9B6', paddingLeft: '60%', cursor: 'pointer' }}
+          >
+            View All
+          </u>
         </a>
       </>
     ) : (
-      <p>"Oh oh! No work due soon!"</p>
-    );
-  };
+      <p style={{ textAlign: 'center' }}>"Oh oh! No work due soon!"</p>
+    )
+  }
   return (
-    <div className={styles.mainwrapper}>
-      <div className={styles.announcementcontainer}>
+    <Row className={styles.mainwrappers}>
+      <Col lg={4} md={12} className={styles.announcementcontainers}>
         <div className="mx-3">
           <Image
             src="/assets/img/crown.svg"
@@ -204,31 +225,15 @@ const Announcesment = ({ goto, classId }) => {
         <div className={styles.upcomingcard}>
           <AssignContent />
         </div>
-      </div>
-      <div className={styles.messagescontainer}>
+      </Col>
+      <Col lg={8} md={12} className={styles.messagescontainers}>
         <div className={styles.firstmessage}>
           <p className="text-dark">Announcements from teacher</p>
         </div>
         <TeacherAnnouncement subject={subject} />
+      </Col>
+    </Row>
+  )
+}
 
-        <div className={styles.posttage} onClick={() => goto(3)}>
-          <Image
-            src="/assets/img/anounceImg.png"
-            alt="teacher"
-            width={50}
-            height={50}
-          />
-          <div className={styles.teachertitle}>
-            <p>
-              Mr Abraham O.(Teacher) posted a new study link: Hello everyone,
-              here is the ...
-            </p>
-            <span>06 Sept 2020</span>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
-};
-
-export default Announcesment;
+export default Announcesment
