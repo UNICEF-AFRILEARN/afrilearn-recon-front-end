aad6669a6730ebe7880bee2064dd2fb5c0c75853
Michael <micheaol80@gmail.com>
2022-11-13 08:29:39 +0100

    Add details for journey

---
+++ components/common/ourjourney/index.jsx
@@ -0,0 +1,178 @@
+import React from 'react';
+
+import styles from './journey.module.css';
+
+const OurJourney = () => {
+  return (
+    <div className={styles.journeymain}>
+        <div className={styles.journeyheader}>
+            <h1>Our Journey</h1>
+        </div>
+        <div className={styles.innerrightcontent}>
+            <div className={styles.rightempty}></div>
+
+            <div className={styles.rightfilled}>
+                <div>
+                    <h4>Apr 2019</h4>
+                </div>
+                <div className={styles.rightfilledcontent}>
+                    <p> We’re incorporated as Afrilearn International Limited</p>
+                </div>
+            </div>
+        </div>
+        <div className={styles.innerrightcontent}>
+            <div className={styles.rightfilled}>
+               
+                <div className={styles.rightfilledcontent}>
+                    <p> 
+                         We get selected for the Orange Corners Business Incubation
+                    </p>
+                </div>
+                <div>
+                    <h4>Feb 2020</h4>
+                </div>
+            </div>
+
+            <div className={styles.rightempty}>
+                
+            </div>
+        </div>
+        <div className={styles.innerrightcontent}>
+            <div className={styles.rightempty}></div>
+
+            <div className={styles.rightfilled}>
+                <div>
+                    <h4>Mar 2020</h4>
+                </div>
+                <div className={styles.rightfilledcontent}>
+                    <p> We emerge 1st of 20,200 businesses in Friends of Prof. Osinbajo SME Competition
+
+                    </p>
+                </div>
+            </div>
+        </div>
+        <div className={styles.innerrightcontent}>
+            <div className={styles.rightfilled}>
+                
+                <div className={styles.rightfilledcontent}>
+                    <p> Our MVP ClassNotes.ng launches, listed #1 Education Platform in Nigeria</p>
+                </div>
+                <div>
+                    <h4>Apr 2020</h4>
+                </div>
+            </div>
+
+            <div className={styles.rightempty}>
+                
+            </div>
+        </div>
+        <div className={styles.innerrightcontent}>
+            <div className={styles.rightempty}></div>
+
+            <div className={styles.rightfilled}>
+                <div className={styles.rightfilleddate}>
+                    <h4>Jun 2020</h4>
+                </div>
+                <div className={styles.rightfilledcontent}>
+                    <p> We win the Netherland’s Business Innovation Fund</p>
+                </div>
+            </div>
+        </div>
+        <div className={styles.innerrightcontent}>
+            <div className={styles.rightfilled}>
+               
+                <div className={styles.rightfilledcontent}>
+                    <p> 
+                    We launch Exambly Mobile App and Web to wide adoption by private and public schools across Nigeria
+                    </p>
+                </div>
+                <div>
+                    <h4>Nov 2020</h4>
+                </div>
+            </div>
+
+            <div className={styles.rightempty}>
+                
+            </div>
+        </div>
+        <div className={styles.innerrightcontent}>
+            <div className={styles.rightempty}></div>
+
+            <div className={styles.rightfilled}>
+                <div>
+                    <h4>Dec 2020</h4>
+                </div>
+                <div className={styles.rightfilledcontent}>
+                    <p > 
+                    We win the U.S. Chamber Digital Innovation Award
+                    </p>
+                </div>
+            </div>
+        </div>
+        <div className={styles.innerrightcontent}>
+            <div className={styles.rightfilled}>
+               
+                <div className={styles.rightfilledcontent}>
+                    <p> 
+                    We get selected for UK-Nigeria Tech Hub’s Top 10 Accelerator
+                    </p>
+                </div>
+                <div>
+                    <h4>Mar 2021</h4>
+                </div>
+            </div>
+
+            <div className={styles.rightempty}>
+                
+            </div>
+        </div>
+        <div className={styles.innerrightcontent}>
+            <div className={styles.rightempty}></div>
+
+            <div className={styles.rightfilled}>
+                <div>
+                    <h4>Apr 2021</h4>
+                </div>
+                <div className={styles.rightfilledcontent}>
+                    <p> 
+                        We get selected for the Future of Work Africa 2021 Accelerator by Village Capital & IFC
+                    </p>
+                </div>
+            </div>
+        </div>
+        <div className={styles.innerrightcontent}>
+            <div className={styles.rightfilled}>
+               
+                <div className={styles.rightfilledcontent}>
+                    <p> 
+                        Afrilearn activates Board of Directors
+                    </p>
+                </div>
+                <div>
+                    <h4>May 2021</h4>
+                </div>
+            </div>
+
+            <div className={styles.rightempty}>
+                
+            </div>
+        </div>
+         <div className={styles.innerrightcontent}>
+            <div className={styles.rightempty}></div>
+
+            <div className={styles.rightfilled}>
+                <div>
+                    <h4>Sept 2021</h4>
+                </div>
+                <div className={styles.rightfilledcontent}>
+                    <p> 
+                    Awarded Changemaker Innovation of the Year 2021 by ACT Foundation.
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default OurJourney
--- pages/about/index.jsx
+++ pages/about/index.jsx
@@ -5,12 +5,14 @@ import AboutHeaders from '../../components/widgets/AboutHeaders/AboutHeaders'
 import AboutUs from '../../components/common/AboutUs';
 import OurLeader from '../../components/common/OurLeader';
 import OurBoard from '../../components/common/ourboard';
+import OurJourney from '../../components/common/ourjourney';
 
 const index = () => {
   return (
     <div className={styles.aboutmainwrapper}>
         <AboutHeaders />
         <AboutUs />
+        <OurJourney />
         <OurLeader />
         <OurBoard />
 
