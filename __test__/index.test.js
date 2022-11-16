957863e1a6c5ffdc817fd55d9d878ca5f1453b21
Michael <micheaol80@gmail.com>
2022-11-16 14:41:27 +0100

    Add test to the student dashboard

--- __test__/index.test.js
+++ __test__/index.test.js
@@ -3,6 +3,7 @@ import Home from '../pages/index';
 import '@testing-library/jest-dom';
 import { Provider } from 'react-redux';
 import { store } from './../redux/store';
+import Dashboard from '../components/features/dashboard/student';
 
 describe('Home', () => {
   it('renders main heading', () => {
@@ -18,4 +19,13 @@ describe('Home', () => {
     const subheading = screen.getByText(/Why Afrilearn works/)
     expect(subheading).toBeInTheDocument()
   })
+});
+
+describe('Dashboard', () => {
+      it('Render recomendation to the screen', () => {
+          render(<Provider store={store}><Dashboard /></Provider>)
+
+          const recommendationElement = screen.getByText(/Recommended For You/)
+          expect(recommendationElement).toBeInTheDocument()
+      })
 });
