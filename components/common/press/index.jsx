e2e480b34c4428b564029787cf03768ecc643cb5
Michael <micheaol80@gmail.com>
2022-11-30 12:45:16 +0100

    Add media link

--- components/common/press/index.jsx
+++ components/common/press/index.jsx
@@ -1,24 +1,10 @@
 import React from 'react';
 
-import { Slide } from 'react-slideshow-image';
-import 'react-slideshow-image/dist/styles.css'
+import Carousel from 'react-bootstrap/Carousel';
+import { pressData } from './pressData'
 
 import styles from './press.module.css'
 
-const slideImages = [
-    {
-      url: '',
-    //   caption: 'Slide 1'
-    },
-    {
-      url: 'images/slide_3.jpg',
-    //   caption: 'Slide 2'
-    },
-    {
-      url: 'images/slide_4.jpg',
-    //   caption: 'Slide 3'
-    },
-  ];
 
   
 const Press = () => {
@@ -34,15 +20,22 @@ const Press = () => {
         <div className={styles.presswrapper}>
         <h2>Press</h2>
         <div className="slide-container">
-        <Slide>
-         {slideImages.map((slideImage, index)=> (
-            <div className="each-slide" key={index}>
-              <div style={{'backgroundImage': `url(${slideImage.url})`}}>
-                {/* <span>{slideImage.caption}</span> */}
-              </div>
-            </div>
-          ))} 
-        </Slide>
+        <Carousel className={styles.curouselwrapper}>
+      {
+        pressData && pressData.map((slide, index) => {
+          return (
+          <Carousel.Item>
+            <img
+              className={styles.slidingimage}
+              src={slide.image}
+              // alt={slide.alt}
+            />
+          </Carousel.Item>
+          )
+        })  
+      }
+        
+    </Carousel>
       </div>
         </div>
     </div>
